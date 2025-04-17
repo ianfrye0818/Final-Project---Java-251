3981a63397f147b7e4313f6c3e70214976e34bc4
Ian Frye <ianfrye.dev@gmail.com>
2025-04-17 17:11:59 -0400

    Cleaning up unused classes and files

---
+++ src/components/TableJPanel.java
@@ -0,0 +1,22 @@
+package components;
+
+import controllers.AppController;
+import enums.ViewType;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TableJPanel extends JPanel {
+    public TableJPanel(AppController controller, JButton viewDetailsButton) {
+        setLayout(new FlowLayout(FlowLayout.CENTER, 20, 15));
+        setBackground(new Color(245, 245, 245));
+
+
+        JButton backButton = new JButton("Back to menu");
+        backButton.addActionListener(e -> controller.setDisplay(ViewType.COFFEE_MENU_VIEW));
+
+        add(backButton);
+        add(viewDetailsButton);
+    }
+}
+
